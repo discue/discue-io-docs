6dfe37adcb44401240697efb2965cce84020d034
stfsy <s.pfaffel@gmail.com>
2022-01-02 23:00:04 +0100

    chore: add tailwind and postcss configs

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./docs/**/*.{vue,js,ts,jsx,tsx,scss}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  corePlugins: {
+    preflight: false,
+  }
+}
