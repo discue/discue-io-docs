3165ccd68343fe86f3913c71de2738fd7f9bf6a1
stfsy <s.pfaffel@gmail.com>
2022-01-06 14:11:20 +0100

    chore: use markdown plugin to normalize headlines and anchors

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,16 +1,55 @@
-/**
- * @typedef {import('@vuepress/theme-default').NavbarConfig} NavbarConfig
- */
+const anchorPlugin = require('markdown-it-anchor')
 
-/**
- * @type NavbarConfig
- */
+// eslint-disable-next-line no-control-regex
+const rControl = /[\u0000-\u001f]/g
+const rSpecial = /[\s~`!@#$%^&*()\-_+=[\]{}|\\;:"'“”‘’<>,.?/]+/g
+const rCombining = /[\u0300-\u036F]/g
+
+const slugify = (str) => {
+    return str
+        .normalize('NFKD')
+        // Remove accents
+        .replace(rCombining, '')
+        // Remove control characters
+        .replace(rControl, '')
+        // Replace special characters
+        .replace(rSpecial, '-')
+        // Remove continuos separators
+        .replace(/-{2,}/g, '-')
+        // Remove prefixing and trailing separators
+        .replace(/^-+|-+$/g, '')
+        // ensure it doesn't start with a number (#121)
+        .replace(/^(\d)/, '_$1')
+        // lowercase
+        .toLowerCase()
+}
+
 module.exports = {
     // site config
     lang: 'en-US',
     title: 'discue',
-    description: 'Documentation of discue.io: Your secure and reliable messaging and queueing service.',
-    markdown: { extractHeaders: { level: [2, 3, 4] } },
+    extendsMarkdown: (md) => {
+        md.use(require('markdown-it-attrs'), {
+            allowedAttributes: ['id'],
+            leftDelimiter: '[',
+            rightDelimiter: ']',
+            allowedAttributes: []  // empty array = all attributes are allowed
+        })
+        md.use(anchorPlugin, {
+            level: [1, 2, 3, 4, 5, 6],
+            slugify,
+            permalink: anchorPlugin.permalink.ariaHidden({
+                class: 'header-anchor',
+                symbol: '#',
+                space: true,
+                placement: 'before',
+            }),
+        })
+    },
+    markdown: {
+        extractHeaders: { level: [2, 3, 4, 5, 6] },
+        anchor: false
+    },
     // theme and its config
     theme: '@vuepress/theme-default',
     themeConfig: {
