de73e355ab063e9acfae3a84dc0fa2f4ec7d8132
Stefan Pfaffel <s.pfaffel@gmail.com>
2022-05-11 11:25:30 +0200

    chore: do not purge css of ui components

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,8 @@ module.exports = {
   content: [
     "./docs/**/*.{vue,js,ts,jsx,tsx,scss,md}",
     "./docs/.vuepress/theme/components/*.{vue,js,ts,jsx,tsx,scss,md}",
+    "./node_modules/@discue/ui-components/dist/ui.common.js",
+    "./node_modules/@discue/ui-components/dist/internal/internal.common.js",
   ],
   theme: {
     extend: {},
