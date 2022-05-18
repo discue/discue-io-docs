a9d18f4afc1076035a41562cf81ea6b503e473b6
Stefan Pfaffel <s.pfaffel@gmail.com>
2022-05-18 22:20:48 +0200

    chore: do not pass template folder

--- generate-api-doc-local.sh
+++ generate-api-doc-local.sh
@@ -4,8 +4,7 @@ set -e
 
 declare -r INTERMEDIATE_FILE="../discue-io-api-docs/docs/api-reference"
 declare -r ENVIRONMENT_FILE="../discue-io-api-docs/generate-api-doc.config.json"
-declare -r TEMPLATE_FOLDER="../discue-io-api-docs/templates"
 
 declare -r API_INPUT_FILE="../discue-io-api/api.yaml"
 
-cd ../widdershins && node widdershins -e "${ENVIRONMENT_FILE}" -o "${INTERMEDIATE_FILE}" -u "${TEMPLATE_FOLDER}"  "${API_INPUT_FILE}" 
+cd ../widdershins && node widdershins -e "${ENVIRONMENT_FILE}" -o "${INTERMEDIATE_FILE}" "${API_INPUT_FILE}" 
