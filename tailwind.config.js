cb3d227ef89194a7edaa966e63020c908b962ef6
Stefan Pfaffel <s.pfaffel@gmail.com>
2022-08-12 06:43:11 +0200

    chore(build): resolve path at runtime

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,8 @@ module.exports = {
   content: [
     "./docs/**/*.{vue,js,ts,jsx,tsx,scss,md}",
     "./docs/.vuepress/theme/components/*.{vue,js,ts,jsx,tsx,scss,md}",
-    "./node_modules/@discue/ui-components/dist/ui.common.js",
-    "./node_modules/@discue/ui-components/dist/internal/internal.common.js",
+    require.resolve('@discue/ui-components'),
+    require.resolve('@discue/ui-components/internal')
   ],
   theme: {
     extend: {},
