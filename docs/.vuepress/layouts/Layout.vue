6ec3edb9100cdc02b3e1328a2b7df45b890dc946
stfsy <s.pfaffel@gmail.com>
2023-01-09 20:21:54 +0100

    docs: add footer to all pages

---
+++ docs/.vuepress/layouts/Layout.vue
@@ -0,0 +1,13 @@
+<script setup>
+import ParentLayout from "@vuepress/theme-default/layouts/Layout.vue";
+</script>
+
+<template>
+  <ParentLayout>
+    <template #page-bottom>
+      <ClientOnly>
+        <Footer :show-logo="false" />
+      </ClientOnly>
+    </template>
+  </ParentLayout>
+</template>
--- docs/.vuepress/theme/index.js
+++ docs/.vuepress/theme/index.js
@@ -6,9 +6,6 @@ export default options => {
     return Object.assign({}, defaultTheme(options), {
         name: 'vuepress-theme-local',
         extends: defaultTheme(options),
-        layouts: {
-            Layout: path.resolve(__dirname, './layouts/Layout.vue')
-        },
         alias: {
             '@theme/AutoLink.vue': path.resolve(__dirname, './components/AutoLinkWithHttpEndpointBadge.vue'),
             '@theme/HomeFooter.vue': path.resolve(__dirname, './layouts/HomeFooter.vue'),
