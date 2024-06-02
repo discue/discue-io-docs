e589c5023568d88a89d07043d0c23b4e10bbf3d1
Stefan Pfaffel <s.pfaffel@gmail.com>
2024-06-02 22:03:57 +0200

    chore: replace footer tag in page meta file

--- run-dev.sh
+++ run-dev.sh
@@ -2,4 +2,8 @@
 
 export NODE_ENV=development
 
+# replace duplicate footer tag
+sed -i 's/<footer/<div/g' ./node_modules/@vuepress/theme-default/lib/client/components/PageMeta.vue
+sed -i 's/<\/footer>/<\/div>/g' ./node_modules/@vuepress/theme-default/lib/client/components/PageMeta.vue
+
 npm run docs:serve
