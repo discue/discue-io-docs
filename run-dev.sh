98c32c54c3bfdb635851f4401d6986095f83fc7c
Stefan Pfaffel <s.pfaffel@gmail.com>
2024-09-09 21:13:34 +0200

    chore: update command to remove duplicate footer

--- run-dev.sh
+++ run-dev.sh
@@ -5,7 +5,7 @@ set -exu
 export NODE_ENV=development
 
 # replace duplicate footer tag
-sed -i 's/<footer/<div/g' ./node_modules/@vuepress/theme-default/lib/client/components/PageMeta.vue
-sed -i 's/<\/footer>/<\/div>/g' ./node_modules/@vuepress/theme-default/lib/client/components/PageMeta.vue
+sed -i 's/<footer/<div/g' ./node_modules/@vuepress/theme-default/lib/client/components/VPPageMeta.vue
+sed -i 's/<\/footer>/<\/div>/g' ./node_modules/@vuepress/theme-default/lib/client/components/VPPageMeta.vue
 
 npm run docs:serve
