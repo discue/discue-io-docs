c1d5c4655650457bed9b0f1e99d2fda24e0fb363
Stefan Pfaffel <s.pfaffel@gmail.com>
2024-09-09 20:59:29 +0200

    chore: do not log theme

--- docs/.vuepress/theme/index.js
+++ docs/.vuepress/theme/index.js
@@ -2,7 +2,6 @@ import { defaultTheme } from '@vuepress/theme-default';
 import path from 'node:path';
 
 export default options => {
-    console.log('theme', JSON.stringify(defaultTheme(options), null, 2))
     return Object.assign({}, defaultTheme(options), {
         name: 'vuepress-theme-local',
         extends: defaultTheme(options),
