5fefb5828a4d19e3a2366a459942809e713b6d83
Stefan Pfaffel <s.pfaffel@gmail.com>
2024-12-01 18:16:53 +0100

    chore: remove broken sidebar override

--- docs/.vuepress/theme/index.js
+++ docs/.vuepress/theme/index.js
@@ -1,12 +1,11 @@
 import { defaultTheme } from '@vuepress/theme-default';
-import path from 'node:path';
 
 export default options => {
     return Object.assign({}, defaultTheme(options), {
         name: 'vuepress-theme-local',
         extends: defaultTheme(options),
         alias: {
-            '@theme/SidebarItem.vue': path.resolve(__dirname, './components/SidebarItem.vue')
+            // '@theme/VPSidebarItem.vue': path.resolve(__dirname, './components/SidebarItem.vue')
         }
     })
 }
