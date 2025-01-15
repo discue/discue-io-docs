0fe19e232ee0b0fa0de0e603f7f7a16844725383
Stefan Pfaffel <s.pfaffel@gmail.com>
2025-01-15 22:18:56 +0100

    feat: use outline plugin

--- docs/.vuepress/configs/plugins.js
+++ docs/.vuepress/configs/plugins.js
@@ -1,5 +1,7 @@
+import outlinePlugin from '@discue/vuepress-plugin-outline'
 import { shikiPlugin } from '@vuepress/plugin-shiki'
 
 export default [
-    shikiPlugin({ theme: 'dark-plus' })
+    shikiPlugin({ theme: 'dark-plus' }),
+    outlinePlugin()
 ]
